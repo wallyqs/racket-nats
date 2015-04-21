53599ea3dff0509c0efbe5b3cba927d3117ca1b9
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2015-04-22 00:45:28 +0900

    Remove line

--- examples/pub-sub.rkt
+++ examples/pub-sub.rkt
@@ -5,9 +5,7 @@
 (define worker-response "{\"result\":~a}~n")
 (define nats-io (nats-connect #:host "127.0.0.1" #:port 4222))
 
-;; Uses the with-nats-io helper to process the protocol
 (printf "Subscribing to 'workers.double ~n")
-
 (nats-sub "workers.double"
           (lambda (msg)
             (define result (* 2 (string->number (bytes->string/utf-8 msg))))
